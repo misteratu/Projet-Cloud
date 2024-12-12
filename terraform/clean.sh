ed0cec0f30bc325c513652995f12d2f575cde732
misteratu <misteratu8@gmail.com>
2024-12-12 08:25:36 +0100

    Fix : clean.sh number of undefined

--- terraform/clean.sh
+++ terraform/clean.sh
@@ -3,8 +3,6 @@
 virsh net-undefine bigdata-net
 virsh undefine bigdata-vm-0
 virsh undefine bigdata-vm-1
-virsh undefine bigdata-vm-2
-virsh undefine bigdata-vm-3
 
 terraform destroy -auto-approve
 terraform refresh
